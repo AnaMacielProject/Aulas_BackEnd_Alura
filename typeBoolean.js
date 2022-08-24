bf1a34b01c940a65b192696009017334d077dffd
AnaMacielProject <ana.maciell@gmail.com>
2022-08-24 11:59:45 -0300

    Arquivos de variaveis do curso alura

--- typeBoolean.js
+++ typeBoolean.js
@@ -6,7 +6,6 @@ console.log( primeiroNumero == segundoNUmero);
 const texto1 = "alura";
 const texto2 = "a";
 
-console.log(texto1 == texto2);
+clearconsole.log(texto1 == texto2);
 
-console.log( length(texto1) == length(texto2));
- 
+
